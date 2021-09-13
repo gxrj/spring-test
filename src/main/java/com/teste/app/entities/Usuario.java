fddece949dea00b05e9cb81b4a62033abc881845
Gustavo de Souza Lemos <gsouzafone@gmail.com>
2021-09-12 22:48:13 -0300

    .

--- src/main/java/com/teste/app/entities/Usuario.java
+++ src/main/java/com/teste/app/entities/Usuario.java
@@ -8,7 +8,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Entity
+@Entity( name= "usuario" )
 public class Usuario {
 
     @Id
@@ -25,7 +25,6 @@ public class Usuario {
     @Column( name = "senha", nullable = false )
     private String senha;
 
-
     protected Usuario() { }
 
     public Usuario( String login, String nome, String senha ) {
@@ -34,7 +33,15 @@ public class Usuario {
         this.senha = senha ;
     }
 
-    public void setSenha( String senha){ this.senha = senha; }
+    public UUID getId() { return id; }
+    public void setId( UUID id ) { this.id = id; }
+    public String getLogin() { return login; }
+    public void setLogin( String login ) { this.login = login; }
+    public String getNome() { return nome; }
+    public void setNome( String nome ) { this.nome = nome; }
+    public String getSenha() { return senha; }
+    public void setSenha( String senha ) { this.senha = senha; }
+    
     @Override
     public String toString(){
         return String.format( "Usuario [ login = %s, nome = %s ]", login, nome );

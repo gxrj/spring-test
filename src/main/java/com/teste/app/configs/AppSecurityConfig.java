40eb30479e011985c77eb253b391d892fdd2714f
Gustavo de Souza Lemos <32879380+gxrj@users.noreply.github.com>
2021-12-29 22:32:02 -0300

    Update AppSecurityConfig.java

--- src/main/java/com/teste/app/configs/AppSecurityConfig.java
+++ src/main/java/com/teste/app/configs/AppSecurityConfig.java
@@ -1,7 +1,10 @@
 package com.teste.app.configs;
 
+import com.teste.app.services.UserAuthenticationService;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.userdateails.UserDetailsService;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -9,6 +12,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 @EnableWebSecurity
 public class AppSecurityConfig{
+    
+    @Bean
+    public UserDetailService getSecurityUserBean(){ return new UserAuthenticationService(); }
     
     @Bean
     public PasswordEncoder getPasswordEncoder(){ return new BCryptPasswordEncoder(); }

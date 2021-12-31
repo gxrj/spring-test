d4dbd2342bcb75fa4e64f122a4bb7a7a6bb8ab3c
Gustavo de Souza Lemos <32879380+gxrj@users.noreply.github.com>
2021-12-30 22:36:39 -0300

    Update ResourceServerConfig.java

--- src/main/java/com/teste/app/configs/oauth2/ResourceServerConfig.java
+++ src/main/java/com/teste/app/configs/oauth2/ResourceServerConfig.java
@@ -1,11 +1,13 @@
 package com.teste.app.configs.oauth2;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
+@Configuration
 public class ResourceServerConfig {
  
     /**
